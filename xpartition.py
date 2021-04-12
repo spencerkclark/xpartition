49206c8cb5cf36a68943859a3312b90b691a0a1d
Noah D. Brenowitz <nbren12@gmail.com>
2021-04-12 16:05:49 -0700

    simplify zeros_like implementation

--- xpartition.py
+++ xpartition.py
@@ -303,17 +303,8 @@ class PartitionDatasetAccessor:
         )
 
 
-def zeros_like_dataarray(arr, chunks):
-    dask_chunks = {
-        arr.get_axis_num(dim): size for dim, size in chunks.items() if dim in arr.dims
-    }
-    return xr.apply_ufunc(
-        dask.array.zeros_like, arr, kwargs=dict(chunks=dask_chunks), dask="allowed"
-    )
-
-
 def zeros_like(ds: xr.Dataset, chunks):
-    return ds.apply(zeros_like_dataarray, chunks=chunks, keep_attrs=True)
+    return xr.zeros_like(ds).chunk(chunks)
 
 
 class _ValidWorkPlan:
